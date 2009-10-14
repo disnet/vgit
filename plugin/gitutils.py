bb7a2c157553db44ddab1ddbad808959464e76de
Tim Disney <tim.disney@gmail.com>
2009-10-14 01:04:15 -0700

    modifying a bit to using mocking library

--- plugin/gitutils.py
+++ plugin/gitutils.py
@@ -1,24 +1,36 @@
 import vim, os
 
-class GitUtils:
+class GitWrapper:
     """Wrapper around git calls"""
-    def __init__(self, vi):
-        self.vi = vi
 
-    def _changes(self, fname):
+    def _whatchanged(self, fname):
         try:
             f = os.popen("git --no-pager whatchanged --pretty=oneline " + fname)
             raw_changes = f.read()
         finally:
             f.close()
 
-    def foo(self):
-        return True
+    def changed_revisions(self, fname):
+        """returns the revisions in which the given file was changed"""
+        raw_revisions = self._whatchanged(fname)
+        if(raw_revisions.startswith("fatal:")):
+            return []
+        else:
+            revisions = raw_revisions.split("\n")
+            return [revisions[i].split(" ")[0] for i in range(0,len(revisions)) \
+                    if i%2 == 0]
+
+    def show_file(self, fname, revision):
+        return ""
+
+
+class GitUtils:
+    """Vim utility methods related to git"""
+    def __init__(self, vi):
+        self.vi = vi
 
     def test():
         name = vim.eval("@%")
-
-        changes = os.popen("git --no-pager whatchanged --pretty=oneline " + name).read()
 
         vim.command("silent keepjumps :open foo")
         vim.command("setlocal bufhidden=delete")
--- plugin/test.py
+++ plugin/test.py
@@ -1,17 +1,39 @@
-import gitutils, vim
-import unittest
+import vim, unittest
+from gitutils import *
+from mock import Mock
 
 class GitUtilsTest(unittest.TestCase):
-
     def setUp(self):
         v = vim.Vim()
-        self.git = gitutils.GitUtils(v)
+        self.git = GitUtils(v)
 
-    def testSimple(self):
-        self.assert_(True)
+class GitWrapperTest(unittest.TestCase):
+    def setUp(self):
+        self.git = GitWrapper()
 
-    def testFoo(self):
-        self.assert_(self.git.foo())
+    def test_changes_for_non_file(self):
+        self.git._whatchanged = Mock(return_value="fatal: ambiguous argument...")
+
+        changed = self.git.changed_revisions("FILE_NOT_REAL")
+        self.assertEquals(len(changed), 0)
+        self.git._whatchanged.assert_called_with("FILE_NOT_REAL")
+
+
+    def test_changes_for_real_file(self):
+        self.git._whatchanged = Mock(return_value="513134261962e58f90a2a88cda2738d1da92cb6d making classes in prep for testing\n:100644 100644 cbf55a2... 5dd6776... M  plugin/vim.py\n48ecba5153ca0b4a44659435a61fa6810815e763 initial import\n:000000 100644 0000000... cbf55a2... A  plugin/vim.py")
+
+        changed = self.git.changed_revisions("real_file.py")
+        self.git._whatchanged.assert_called_with("real_file.py")
+        self.assertEquals(len(changed),2)
+        self.assertEquals(changed[0],"513134261962e58f90a2a88cda2738d1da92cb6d")
+
+    def test_show_file_dne(self):
+        self.git._showfile = Mock(return_value="fatal: ambiguous argument...")
+
+        file = self.git.show_file("FILE_NOT_REAL", "123")
+
+        self.assertEqual(len(file), 0)
+        self.git._showfile.assert_called_with("123:FILE_NOT_REAL")
 
 
 
