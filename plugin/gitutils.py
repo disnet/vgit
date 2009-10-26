cbf046bc289365db33b82bdac025a2d9f2a08cdc
Tim Disney <tim.disney@gmail.com>
2009-10-26 00:29:24 -0700

    passing tests for show_file and changed_revisions

--- plugin/gitutils.py
+++ plugin/gitutils.py
@@ -1,20 +1,30 @@
 import vim, os
 
+class GitUtilsError(Exception):
+    pass
+
+class GitUtilsInputError(GitUtilsError):
+    def __init__(self, message, git_message):
+        self.message = message
+        self.git_message = git_message
+
 class GitWrapper:
     """Wrapper around git calls"""
 
-    def _whatchanged(self, fname):
+    def _whatchanged(self, object):
+        """runs git whatchanged to get a list of revisions for given git object"""
         try:
-            f = os.popen("git --no-pager whatchanged --pretty=oneline " + fname)
+            f = os.popen("git --no-pager whatchanged --pretty=oneline " + object)
             raw_changes = f.read()
         finally:
             f.close()
 
         return raw_changes
 
-    def _showfile(self, arg):
+    def _showfile(self, object):
+        """runs git show to retrieve contents of given git object"""
         try:
-            f = os.popen("git --no-pager show %s" % arg)
+            f = os.popen("git --no-pager show %s" % object)
             raw_changes = f.read()
         finally:
             f.close()
@@ -26,15 +36,19 @@ class GitWrapper:
         """returns the revisions in which the given file was changed"""
         raw_revisions = self._whatchanged(fname)
         if(raw_revisions.startswith("fatal:")):
-            return []
+            raise GitUtilsInputError("File does not exist", raw_revisions)
         else:
             revisions = raw_revisions.split("\n")
             return [revisions[i].split(" ")[0] for i in range(0,len(revisions)) \
                     if i%2 == 0]
 
     def show_file(self, fname, revision):
-        return ""
-
+        """returns the contents of the given file at the given revision"""
+        contents = self._showfile("%s:%s" % (revision, fname) )
+        if(contents.startswith("fatal:")):
+            raise GitUtilsInputError("File at revision does not exist", contents)
+        else:
+            return contents
 
 class GitUtils:
     """Vim utility methods related to git"""
--- plugin/test.py
+++ plugin/test.py
@@ -12,15 +12,20 @@ class GitWrapperTest(unittest.TestCase):
         self.git = GitWrapper()
 
     def test_changes_for_non_file(self):
+        """make sure fake files throw exceptions"""
         self.git._whatchanged = Mock(return_value="fatal: ambiguous argument...")
-
-        changed = self.git.changed_revisions("FILE_NOT_REAL")
-        self.assertEquals(len(changed), 0)
+        self.assertRaises(GitUtilsInputError, self.git.changed_revisions, "FILE_NOT_REAL")
         self.git._whatchanged.assert_called_with("FILE_NOT_REAL")
 
 
     def test_changes_for_real_file(self):
-        self.git._whatchanged = Mock(return_value="513134261962e58f90a2a88cda2738d1da92cb6d making classes in prep for testing\n:100644 100644 cbf55a2... 5dd6776... M  plugin/vim.py\n48ecba5153ca0b4a44659435a61fa6810815e763 initial import\n:000000 100644 0000000... cbf55a2... A  plugin/vim.py")
+        """find list of changes for "real" files"""
+        mock_return = """513134261962e58f90a2a88cda2738d1da92cb6d making classes in prep for testing
+:100644 100644 cbf55a2... 5dd6776... M  plugin/vim.py
+48ecba5153ca0b4a44659435a61fa6810815e763 initial import
+:000000 100644 0000000... cbf55a2... A  plugin/vim.py")"""
+
+        self.git._whatchanged = Mock(return_value=mock_return)
 
         changed = self.git.changed_revisions("real_file.py")
         self.git._whatchanged.assert_called_with("real_file.py")
@@ -28,14 +33,19 @@ class GitWrapperTest(unittest.TestCase):
         self.assertEquals(changed[0],"513134261962e58f90a2a88cda2738d1da92cb6d")
 
     def test_show_file_dne(self):
+        """make sure fake files raise exceptions"""
         self.git._showfile = Mock(return_value="fatal: ambiguous argument...")
-
-        file = self.git.show_file("FILE_NOT_REAL", "123")
-
-        self.assertEqual(len(file), 0)
+        self.assertRaises(GitUtilsError, self.git.show_file, "FILE_NOT_REAL", "123")
         self.git._showfile.assert_called_with("123:FILE_NOT_REAL")
 
+    def test_show_file_real(self):
+        """now display a real file at some given revisions"""
+        mock_return = "here's some example text"
+        self.git._showfile = Mock(return_value=mock_return)
+        file = self.git.show_file("real.txt", "123")
 
+        self.assertEquals(file, mock_return)
+        self.git._showfile.assert_called_with("123:real.txt")
 
 
 if __name__ == "__main__":
