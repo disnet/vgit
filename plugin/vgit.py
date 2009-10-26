7de3fc5a1c05a91805ace04642aa103bb64be61c
Tim Disney <tim.disney@gmail.com>
2009-10-26 01:26:56 -0700

    think I don't need class structure

--- plugin/vgit.py
+++ plugin/vgit.py
@@ -1,27 +1,21 @@
 from vim import eval, command
 from gitwrapper import *
 
-class GitUtils:
-    """Vim utility methods related to git"""
-    def working_file():
-        return eval("@%")
-
-    def test():
-        name = self.working_file()
-
-        command("silent keepjumps :open foo")
-        command("setlocal bufhidden=delete")
-        command("setlocal buftype=nofile")
-        command("setlocal modifiable")
-        command("setlocal noswapfile")
-        command("setlocal nowrap")
-
-        output = os.popen("git --no-pager show HEAD:" + name).read()
-        cb = vim.current.buffer
-        for gline in output.split("\n"):
-            cb.append(gline)
-
+def _working_file():
+    return eval("@%")
 
 def GitPreviousRevision():
-    pass
+    name = _working_file()
+
+    command("silent keepjumps :open foo")
+    command("setlocal bufhidden=delete")
+    command("setlocal buftype=nofile")
+    command("setlocal modifiable")
+    command("setlocal noswapfile")
+    command("setlocal nowrap")
+
+    output = os.popen("git --no-pager show HEAD:" + name).read()
+    cb = vim.current.buffer
+    for gline in output.split("\n"):
+        cb.append(gline)
 
