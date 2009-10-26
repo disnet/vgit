3ff28084df80cad5366997d562da5e6fba886133
Tim Disney <tim.disney@gmail.com>
2009-10-26 01:17:52 -0700

    moved vim specific code out into separate file

--- plugin/gitwrapper.py
+++ plugin/gitwrapper.py
@@ -59,32 +59,3 @@ class GitWrapper:
             raise GitUtilsInputError("File at revision does not exist", contents)
         else:
             return contents
-
-class GitUtils:
-    """Vim utility methods related to git"""
-    def __init__(self, vi):
-        self.vi = vi
-
-    def test():
-        name = vim.eval("@%")
-
-        vim.command("silent keepjumps :open foo")
-        vim.command("setlocal bufhidden=delete")
-        vim.command("setlocal buftype=nofile")
-        vim.command("setlocal modifiable")
-        vim.command("setlocal noswapfile")
-        vim.command("setlocal nowrap")
-
-        output = os.popen("git --no-pager show HEAD:" + name).read()
-        cb = vim.current.buffer
-        for gline in output.split("\n"):
-            cb.append(gline)
-
-
-def Dtest():
-    v = vim.Vim()
-    git = GitUtils(v)
-
-v = vim.Vim()
-
-v.current.buffer.append("foo")
