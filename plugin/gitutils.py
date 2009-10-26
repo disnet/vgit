6b1bc98af6b702553e63293ffe95d2b8b3ef445a
Tim Disney <tim.disney@gmail.com>
2009-10-26 01:00:31 -0700

    needed a root dir prefix for git show to work in subdirs...also needed to do some whitespace stripping

--- plugin/gitutils.py
+++ plugin/gitutils.py
@@ -31,6 +31,16 @@ class GitWrapper:
 
         return raw_changes
 
+    def _getprefix(self):
+        """gets the path of the current directory relative to the top-level 
+        dir (aka the one with .git). Needed because some commands (git show rev:file) 
+        in git require full path from the project's root directory"""
+        try:
+            f = os.popen("git rev-parse --show-prefix")
+            prefix = f.read().strip()
+        finally:
+            f.close()
+        return prefix
 
     def changed_revisions(self, fname):
         """returns the revisions in which the given file was changed"""
@@ -38,13 +48,13 @@ class GitWrapper:
         if(raw_revisions.startswith("fatal:")):
             raise GitUtilsInputError("File does not exist", raw_revisions)
         else:
-            revisions = raw_revisions.split("\n")
+            revisions = raw_revisions.strip().split("\n")
             return [revisions[i].split(" ")[0] for i in range(0,len(revisions)) \
                     if i%2 == 0]
 
     def show_file(self, fname, revision):
         """returns the contents of the given file at the given revision"""
-        contents = self._showfile("%s:%s" % (revision, fname) )
+        contents = self._showfile("%s:%s" % (revision, self._getprefix() + fname) )
         if(contents.startswith("fatal:")):
             raise GitUtilsInputError("File at revision does not exist", contents)
         else:
