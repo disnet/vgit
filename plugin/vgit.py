b68cba4b85efde7559c5c292305c8f8a767c8d9a
Tim Disney <tim.disney@gmail.com>
2009-10-26 21:58:08 -0700

    filling out functions for vim

--- plugin/vgit.py
+++ plugin/vgit.py
@@ -1,21 +1,120 @@
-from vim import eval, command
-from gitwrapper import *
+import os, vim
+
+class GitUtilsError(Exception):
+    pass
+
+class GitUtilsInputError(GitUtilsError):
+    def __init__(self, message, git_message):
+        self.message = message
+        self.git_message = git_message
+
+class GitWrapper:
+    """Wrapper around git calls"""
+
+    def _whatchanged(self, object):
+        """runs git whatchanged to get a list of revisions for given git object"""
+        try:
+            f = os.popen("git --no-pager whatchanged --pretty=oneline " + object)
+            raw_changes = f.read()
+        finally:
+            f.close()
+
+        return raw_changes
+
+    def _showfile(self, object):
+        """runs git show to retrieve contents of given git object"""
+        try:
+            f = os.popen("git --no-pager show %s" % object)
+            raw_changes = f.read()
+        finally:
+            f.close()
+
+        return raw_changes
+
+    def _getprefix(self):
+        """gets the path of the current directory relative to the top-level 
+        dir (aka the one with .git). Needed because some commands (git show rev:file) 
+        in git require full path from the project's root directory"""
+        try:
+            f = os.popen("git rev-parse --show-prefix")
+            prefix = f.read().strip()
+        finally:
+            f.close()
+        return prefix
+
+    def changed_revisions(self, fname):
+        """returns the revisions in which the given file was changed"""
+        raw_revisions = self._whatchanged(fname)
+        if(raw_revisions.startswith("fatal:")):
+            raise GitUtilsInputError("File does not exist", raw_revisions)
+        else:
+            revisions = raw_revisions.strip().split("\n")
+            return [revisions[i].split(" ")[0] for i in range(0,len(revisions)) \
+                    if i%2 == 0]
+
+    def show_file(self, fname, revision):
+        """returns the contents of the given file at the given revision"""
+        contents = self._showfile("%s:%s" % (revision, self._getprefix() + fname) )
+        if(contents.startswith("fatal:")):
+            raise GitUtilsInputError("File at revision does not exist", contents)
+        else:
+            return contents
 
 def _working_file():
-    return eval("@%")
+    return vim.eval("@%")
 
 def GitPreviousRevision():
+    git = GitWrapper()
     fname = _working_file()
 
-    command("silent keepjumps :open foo")
-    command("setlocal bufhidden=delete")
-    command("setlocal buftype=nofile")
-    command("setlocal modifiable")
-    command("setlocal noswapfile")
-    command("setlocal nowrap")
+    if(fname.find(":") > 0): # we're already in one of the previous revision buffers
+        (fname, current_rev) = fname.split(":")
+        revisions = git.changed_revisions(fname)
+        
+        current_index = revisions.index(current_rev)
+        if(current_index >= len(revisions) - 1):
+            print "Earliest revision"
+            return
+        else:
+            # so figure out what the current revision is and chose the next one
+            rev = revisions[revisions.index(current_rev) + 1] 
+    else:
+        revisions = git.changed_revisions(fname)
+        rev = revisions[1]
 
-    output = os.popen("git --no-pager show HEAD:" + fname).read()
+    vim.command("silent keepjumps :open %s:%s" % (fname, rev))
+    vim.command("setlocal buftype=nofile")
+    vim.command("setlocal modifiable")
+
+    contents = git.show_file(fname, rev)
     cb = vim.current.buffer
-    for gline in output.split("\n"):
+    for gline in contents.split("\n"):
         cb.append(gline)
 
+def GitNextRevision():
+    git = GitWrapper()
+    fname = _working_file()
+
+    if(fname.find(":") > 0):
+        (fname, current_rev) = fname.split(":")
+        revisions = git.changed_revisions(fname)
+        
+        current_index = revisions.index(current_rev)
+        if(current_index == 0):
+            print "Latest revision"
+            return
+        else:
+            # so figure out what the current revision is and chose the next one
+            rev = revisions[revisions.index(current_rev) - 1] 
+    else:
+        print "Not in git vevision buffer -- call GitPreviousRevision first"
+        return
+
+    vim.command("silent keepjumps :open %s:%s" % (fname, rev))
+    vim.command("setlocal buftype=nofile")
+    vim.command("setlocal modifiable")
+
+    contents = git.show_file(fname, rev)
+    cb = vim.current.buffer
+    for gline in contents.split("\n"):
+        cb.append(gline)
